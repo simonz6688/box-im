d689df119d826b17f702cc5b4f6c27af0da39a17
WuZeyue <1109607743@qq.com>
2023-04-02 16:28:35 +0800

    优化注册页面

--- im-ui/src/view/Register.vue
+++ im-ui/src/view/Register.vue
@@ -1,31 +1,33 @@
 <template>
-	<el-container class="register-view">
-		<div>
-			
-			<el-form :model="registerForm" status-icon :rules="rules" ref="registerForm" label-width="80px" class="web-ruleForm">
-				<div class="register-brand">欢迎注册</div>
-				<el-form-item label="用户名" prop="userName">
-					<el-input type="userName" v-model="registerForm.userName" autocomplete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="昵称" prop="nickName">
-					<el-input type="nickName" v-model="registerForm.nickName" autocomplete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="密码" prop="password">
-					<el-input type="password" v-model="registerForm.password" autocomplete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="确认密码" prop="confirmPassword">
-					<el-input type="password" v-model="registerForm.confirmPassword" autocomplete="off"></el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="submitForm('registerForm')">注册</el-button>
-					<el-button @click="resetForm('registerForm')">清空</el-button>
-				</el-form-item>
-				<div class="to-login">
-					<router-link to="/login">已有账号,前往登录</router-link>
-				</div>
-			</el-form>
-		</div>
-	</el-container>
+	<el-row  type="flex" justify="center" class="register-container">
+		<el-col :span="6">
+			<el-card class="register-card" shadow="always">
+				<div slot="header" class="register-brand" style="font-style: italic;font-size: 30px; margin: 5px;">欢迎注册</div>
+				<el-form :model="registerForm" status-icon :rules="rules" ref="registerForm" label-width="80px" class="web-ruleForm">
+					
+					<el-form-item label="用户名" prop="userName" class="form-item">
+						<el-input type="userName" v-model="registerForm.userName" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="昵称" prop="nickName" class="form-item">
+						<el-input type="nickName" v-model="registerForm.nickName" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="密码" prop="password" class="form-item">
+						<el-input type="password" v-model="registerForm.password" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item label="确认密码" prop="confirmPassword" class="form-item">
+						<el-input type="password" v-model="registerForm.confirmPassword" autocomplete="off"></el-input>
+					</el-form-item>
+					<el-form-item class="form-button">
+						<el-button type="primary" @click="submitForm('registerForm')">注册</el-button>
+						<el-button @click="resetForm('registerForm')">清空</el-button>
+					</el-form-item>
+					<div class="to-login">
+						<router-link to="/login">已有账号,前往登录</router-link>
+					</div>
+				</el-form>
+			</el-card>
+		</el-col>
+	</el-row>
 </template>
 
 <script>
@@ -113,50 +115,33 @@
 </script>
 
 <style scoped lang="scss">
-	.register-view {
-		position: fixed;
-		display: flex;
-		justify-content: space-around;
-		width: 100%;
-		height: 100%;
-		background: #466368;
-		background: linear-gradient(#65807a, #182e3c); 
-		background-size: cover;
-		-webkit-user-select: none;
-		background-size: cover;
-		
-		
+.register-container{
+	background:  linear-gradient(#c1fcf5, #124d4d); 
+	background-size: cover;
+	height: 100%;
+	.register-card {
+		margin-top: 6vw;
+		border-radius: 0px 70px;
 		.web-ruleForm {
-			width: 500px;
-			height: 430px;
-			padding: 20px;
-			margin-top: 100px ;
-			background: rgba(255,255,255,.75);
-			box-shadow: 0px 0px  1px #ccc;
-			border-radius: 3px;
+			height: 435px;
+			padding: 5px;
 			overflow: hidden;
-			
-			.register-brand {
-				line-height: 50px;
-				margin: 20px 0 30px 0;
-				font-size: 22px;
-				font-weight: 600;
-				letter-spacing: 2px;
-				text-align: center;
-				text-transform: uppercase;
+			.form-item{
+				margin-right: 40px;
+				margin-top: 30px;
 			}
-			
-			.to-login {
-				display: flex;
-				flex-direction: row-reverse;
-				line-height: 40px;
-				text-align: left;
-				padding-left: 20px;
+			.form-button{
+				margin-left: 180px;
+				margin-top: 50px;
+			}
+			.to-login{
+				margin-left: 270px;
+				margin-top: 40px;
 			}
 		}
 	}
 
-	
+}
 
 	
 </style>
